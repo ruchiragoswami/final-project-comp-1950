4f6aac94c04ea3d312d0140f03dad45abadad3dc
Ruchi Goswami <ruchigoswami8266@gmail.com>
2020-07-06 07:10:45 -0700

    new js file added for thank you message. Some CSS applied to contact page.

---
+++ scripts/contact.js
@@ -0,0 +1,7 @@
+window.onload = function() { 
+var f = document.forms["contact-form"];
+
+f.addEventListener("submit" , function() {
+    alert("Thank you for submitting the form. Someone will get back to you in 3 business days.");
+});
+};
